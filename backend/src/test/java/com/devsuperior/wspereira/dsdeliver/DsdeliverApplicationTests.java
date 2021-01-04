193c062f2087d811f9d06c3f92e431aa96d3a4e6
ws-pereira <ws-pereira@hotmail.com>
2021-01-04 11:31:51 -0300

    Create project

---
+++ backend/src/test/java/com/devsuperior/wspereira/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.wspereira.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
