d26ec0d90d3b4d983c052a9998e37183d0d36ab3
ws-pereira <ws-pereira@hotmail.com>
2021-01-08 18:34:15 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,12 @@
-
 import './App.css';
-import Hello from './Hello';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div>
-      <Hello message = "Meu primeiro React" />
-      <Hello message = "Tentando aprender cooisas nova" />
-
-
+      <Navbar />
     </div>
+
   );
 }
 
